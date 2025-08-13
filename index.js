6e25990142c83dc455d3635d41a45e955fbd04e7
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-08-14 02:54:20 +0600

    Filtering api added

--- index.js
+++ index.js
@@ -7,25 +7,59 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 
+function getLatestPrice(prices) {
+    let latest = prices[0];
+
+    for (let p of prices) {
+        const [dateStr] = p.split(" | ");
+        const date = new Date(dateStr.split('/').reverse().join('-')); // dd/mm/yyyy to yyyy-mm-dd
+        const latestDate = new Date(latest.split('|')[0].split('/').reverse().join('-'));
+        if (date > latestDate) latest = p;
+    }
+    return parseFloat(latest.split('|')[1].trim());
+
+}
+
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 })
 
 app.get("/api/houses", (req, res) => {
-    const page = parseInt(req.query.page) || 1;
-    const limit = 200;
+    let {
+        minLat, maxLat, minLng, maxLng,
+        listingType, minPrice, maxPrice, homeType, beds, baths
+    } = req.query;
 
-    const startIdx = (page - 1) * limit;
-    const endIdx = page * limit;
+    // Conver query params to actual value
+    minLat = minLat ? parseFloat(minLat) : -90;
+    maxLat = maxLat ? parseFloat(maxLat) : 90;
+    minLng = minLng ? parseFloat(minLng) : -180;
+    maxLng = maxLng ? parseFloat(maxLng) : 180;
+    minPrice = minPrice ? parseFloat(minPrice) : 0;
+    maxPrice = maxPrice ? parseFloat(maxPrice) : Infinity;
+    beds = beds ? parseInt(beds) : 0;
+    baths = baths ? parseInt(baths) : 0;
 
-    const result = houses.slice(startIdx, endIdx);
+    const filtered = houses.filter(h => {
+        const price = getLatestPrice(h.prices);
 
-    res.json({
-        page,
-        totalPages: Math.ceil(houses.length / limit),
-        data: result
+        return h.lat >= minLat &&
+            h.lat <= maxLat &&
+            h.lon >= minLng &&
+            h.lon <= maxLng &&
+            price >= minPrice &&
+            price <= maxPrice &&
+            h.beds >= beds &&
+            h.baths >= baths &&
+            (!listingType || h.listingType === listingType) &&
+            (!homeType || h.homeType === homeType);
     });
 
+    res.json({
+        count: filtered.length,
+        data: filtered
+    })
 })
 
 app.listen(port, () => {
