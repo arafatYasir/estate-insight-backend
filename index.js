f2864a9534394d54c5a99a963cc5e33ded79873e
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-08-15 03:43:07 +0600

    Code cleanup

--- index.js
+++ index.js
@@ -104,12 +104,10 @@ app.get("/api/houses", (req, res) => {
 
     const zoomLimit = getZoomBasedLimit(zoomLevel);
 
-    if(zoomLimit) {
+    if(zoomLimit && filtered.length > zoomLimit) {
         filtered.sort((a, b) => b.latestPrice - a.latestPrice);
 
         filtered = filtered.slice(0, zoomLimit);
-
-        console.log(`Zoom Level: ${zoomLevel}, Applied Limit: ${zoomLimit}, Results: ${filtered.length}`)
     }
 
     const result = limit ? filtered.slice(0, limit) : filtered;
